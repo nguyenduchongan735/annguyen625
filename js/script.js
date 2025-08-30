074fbf012207c8544f583ffded441f747bbe3f00
nguyenduchongan735 <nguyenduchongan735@gmail.com>
2025-08-30 18:15:38 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
+setInterval(nextSlide, 4000); // đổi ảnh mỗi 4 giây
